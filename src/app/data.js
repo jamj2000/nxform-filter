b1e875323e91d2d075c9e73839730d9ec6960289
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-05-22 13:03:12 +0200

    update data

--- src/app/data.js
+++ src/app/data.js
@@ -1,56 +1,73 @@
 export const tShirts = [
+  // Nike
   { id: 1, size: "S", color: "red", price: 19.99, brand: "Nike" },
-  { id: 2, size: "M", color: "blue", price: 24.99, brand: "Adidas" },
-  { id: 3, size: "L", color: "green", price: 22.5, brand: "Puma" },
-  { id: 4, size: "XL", color: "black", price: 18.75, brand: "Under Armour" },
-  { id: 5, size: "M", color: "white", price: 20.0, brand: "Reebok" },
-  { id: 6, size: "S", color: "yellow", price: 17.99, brand: "H&M" },
-  { id: 7, size: "L", color: "purple", price: 21.0, brand: "Zara" },
-  { id: 8, size: "XL", color: "gray", price: 23.5, brand: "Uniqlo" },
-  { id: 9, size: "M", color: "orange", price: 19.5, brand: "Levi's" },
-  { id: 10, size: "L", color: "pink", price: 25.0, brand: "Guess" },
+  { id: 2, size: "M", color: "blue", price: 21.99, brand: "Nike" },
+  { id: 3, size: "L", color: "black", price: 23.99, brand: "Nike" },
+  { id: 4, size: "XL", color: "white", price: 22.49, brand: "Nike" },
 
-  { id: 11, size: "S", color: "navy", price: 16.99, brand: "Nike" },
-  { id: 12, size: "M", color: "maroon", price: 24.49, brand: "Adidas" },
-  { id: 13, size: "L", color: "lime", price: 21.5, brand: "Puma" },
-  { id: 14, size: "XL", color: "charcoal", price: 19.75, brand: "Under Armour" },
-  { id: 15, size: "M", color: "beige", price: 20.5, brand: "Reebok" },
-  { id: 16, size: "S", color: "aqua", price: 18.99, brand: "H&M" },
-  { id: 17, size: "L", color: "burgundy", price: 22.0, brand: "Zara" },
-  { id: 18, size: "XL", color: "olive", price: 23.0, brand: "Uniqlo" },
-  { id: 19, size: "M", color: "teal", price: 20.99, brand: "Levi's" },
-  { id: 20, size: "L", color: "coral", price: 26.0, brand: "Guess" },
+  // Adidas
+  { id: 5, size: "S", color: "green", price: 20.99, brand: "Adidas" },
+  { id: 6, size: "M", color: "gray", price: 22.99, brand: "Adidas" },
+  { id: 7, size: "L", color: "red", price: 24.99, brand: "Adidas" },
+  { id: 8, size: "XL", color: "blue", price: 19.49, brand: "Adidas" },
 
-  { id: 21, size: "S", color: "silver", price: 19.99, brand: "Nike" },
-  { id: 22, size: "M", color: "gold", price: 24.99, brand: "Adidas" },
-  { id: 23, size: "L", color: "ivory", price: 22.5, brand: "Puma" },
-  { id: 24, size: "XL", color: "cyan", price: 18.75, brand: "Under Armour" },
-  { id: 25, size: "M", color: "magenta", price: 20.0, brand: "Reebok" },
-  { id: 26, size: "S", color: "brown", price: 17.99, brand: "H&M" },
-  { id: 27, size: "L", color: "crimson", price: 21.0, brand: "Zara" },
-  { id: 28, size: "XL", color: "indigo", price: 23.5, brand: "Uniqlo" },
-  { id: 29, size: "M", color: "peach", price: 19.5, brand: "Levi's" },
-  { id: 30, size: "L", color: "mint", price: 25.0, brand: "Guess" },
+  // Puma
+  { id: 9, size: "S", color: "black", price: 18.99, brand: "Puma" },
+  { id: 10, size: "M", color: "white", price: 20.99, brand: "Puma" },
+  { id: 11, size: "L", color: "green", price: 22.49, brand: "Puma" },
+  { id: 12, size: "XL", color: "gray", price: 23.99, brand: "Puma" },
 
-  { id: 31, size: "S", color: "red", price: 19.99, brand: "Nike" },
-  { id: 32, size: "M", color: "blue", price: 24.99, brand: "Adidas" },
-  { id: 33, size: "L", color: "green", price: 22.5, brand: "Puma" },
-  { id: 34, size: "XL", color: "black", price: 18.75, brand: "Under Armour" },
-  { id: 35, size: "M", color: "white", price: 20.0, brand: "Reebok" },
-  { id: 36, size: "S", color: "yellow", price: 17.99, brand: "H&M" },
-  { id: 37, size: "L", color: "purple", price: 21.0, brand: "Zara" },
-  { id: 38, size: "XL", color: "gray", price: 23.5, brand: "Uniqlo" },
-  { id: 39, size: "M", color: "orange", price: 19.5, brand: "Levi's" },
-  { id: 40, size: "L", color: "pink", price: 25.0, brand: "Guess" },
+  // Reebok
+  { id: 13, size: "S", color: "red", price: 21.49, brand: "Reebok" },
+  { id: 14, size: "M", color: "blue", price: 20.00, brand: "Reebok" },
+  { id: 15, size: "L", color: "black", price: 22.00, brand: "Reebok" },
+  { id: 16, size: "XL", color: "white", price: 23.50, brand: "Reebok" },
 
-  { id: 41, size: "S", color: "navy", price: 16.99, brand: "Nike" },
-  { id: 42, size: "M", color: "maroon", price: 24.49, brand: "Adidas" },
-  { id: 43, size: "L", color: "lime", price: 21.5, brand: "Puma" },
-  { id: 44, size: "XL", color: "charcoal", price: 19.75, brand: "Under Armour" },
-  { id: 45, size: "M", color: "beige", price: 20.5, brand: "Reebok" },
-  { id: 46, size: "S", color: "aqua", price: 18.99, brand: "H&M" },
-  { id: 47, size: "L", color: "burgundy", price: 22.0, brand: "Zara" },
-  { id: 48, size: "XL", color: "olive", price: 23.0, brand: "Uniqlo" },
-  { id: 49, size: "M", color: "teal", price: 20.99, brand: "Levi's" },
-  { id: 50, size: "L", color: "coral", price: 26.0, brand: "Guess" }
+  // Under Armour
+  { id: 17, size: "S", color: "green", price: 19.75, brand: "Under Armour" },
+  { id: 18, size: "M", color: "gray", price: 21.00, brand: "Under Armour" },
+  { id: 19, size: "L", color: "red", price: 22.25, brand: "Under Armour" },
+  { id: 20, size: "XL", color: "blue", price: 24.00, brand: "Under Armour" },
+
+  // H&M
+  { id: 21, size: "S", color: "black", price: 17.99, brand: "H&M" },
+  { id: 22, size: "M", color: "white", price: 18.99, brand: "H&M" },
+  { id: 23, size: "L", color: "green", price: 19.99, brand: "H&M" },
+  { id: 24, size: "XL", color: "gray", price: 21.49, brand: "H&M" },
+
+  // Zara
+  { id: 25, size: "S", color: "red", price: 20.00, brand: "Zara" },
+  { id: 26, size: "M", color: "blue", price: 21.50, brand: "Zara" },
+  { id: 27, size: "L", color: "black", price: 22.00, brand: "Zara" },
+  { id: 28, size: "XL", color: "white", price: 23.00, brand: "Zara" },
+
+  // Uniqlo
+  { id: 29, size: "S", color: "green", price: 20.00, brand: "Uniqlo" },
+  { id: 30, size: "M", color: "gray", price: 21.00, brand: "Uniqlo" },
+  { id: 31, size: "L", color: "red", price: 22.00, brand: "Uniqlo" },
+  { id: 32, size: "XL", color: "blue", price: 23.00, brand: "Uniqlo" },
+
+  // Levi's
+  { id: 33, size: "S", color: "black", price: 19.50, brand: "Levi's" },
+  { id: 34, size: "M", color: "white", price: 20.50, brand: "Levi's" },
+  { id: 35, size: "L", color: "green", price: 21.50, brand: "Levi's" },
+  { id: 36, size: "XL", color: "gray", price: 22.50, brand: "Levi's" },
+
+  // Guess
+  { id: 37, size: "S", color: "red", price: 25.00, brand: "Guess" },
+  { id: 38, size: "M", color: "blue", price: 24.50, brand: "Guess" },
+  { id: 39, size: "L", color: "black", price: 23.99, brand: "Guess" },
+  { id: 40, size: "XL", color: "white", price: 24.99, brand: "Guess" },
+
+  // Segunda ronda para completar 50
+  { id: 41, size: "S", color: "green", price: 20.49, brand: "Nike" },
+  { id: 42, size: "M", color: "gray", price: 19.75, brand: "Adidas" },
+  { id: 43, size: "L", color: "red", price: 22.49, brand: "Puma" },
+  { id: 44, size: "XL", color: "blue", price: 21.99, brand: "Reebok" },
+  { id: 45, size: "S", color: "black", price: 23.25, brand: "Under Armour" },
+  { id: 46, size: "M", color: "white", price: 19.75, brand: "H&M" },
+  { id: 47, size: "L", color: "green", price: 22.25, brand: "Zara" },
+  { id: 48, size: "XL", color: "gray", price: 20.99, brand: "Uniqlo" },
+  { id: 49, size: "S", color: "red", price: 21.00, brand: "Levi's" },
+  { id: 50, size: "M", color: "blue", price: 24.49, brand: "Guess" },
 ];
